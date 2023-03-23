e8df0b04db2fbc21e1babfbeed76a4b5124b85d2
d0422 <rlfehd2013@naver.com>
2023-03-23 11:18:04 +0900

    feat: 각 자리수를 모두 더해주는 기능 추가

--- JavaScript/onBoarding/이동길/src/problem1.js
+++ JavaScript/onBoarding/이동길/src/problem1.js
@@ -4,3 +4,13 @@ function problem1(pobi, crong) {
 }
 
 module.exports = problem1;
+
+function getAddedNumber(number) {
+  const numberArray = getSliceNumberArray(number);
+  return numberArray.reduce((sum, num) => sum + num);
+}
+function getSliceNumberArray(number) {
+  const str = String(number);
+  const stringArray = str.split('');
+  return stringArray.map((str) => Number(str));
+}
