238492864a5207896a5018b87ceae61dca00f28d
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-03-28 03:43:35 +0900

    [Feat] 각 자리의 숫자로 쪼개 배열에 넣기

--- JavaScript/onBoarding/정지원/src/problem3.js
+++ JavaScript/onBoarding/정지원/src/problem3.js
@@ -1,7 +1,14 @@
 function problem3(number) {
   for(let i=1; i<number+1; i++){
-    
+    const numArray = makeNumEachToArray(i);
   }
+}
+
+function makeNumEachToArray(inputNum) {
+  return inputNum
+    .toString()
+    .split("")
+    .map((x) => parseInt(x));
 }
 
 module.exports = problem3;
