9d61dc31d25b1c64eadd5d71b0dac92ab4f843a7
d0422 <rlfehd2013@naver.com>
2023-03-27 17:45:14 +0900

    feat: 예외사항 구현

--- JavaScript/onBoarding/이동길/src/problem2.js
+++ JavaScript/onBoarding/이동길/src/problem2.js
@@ -1,6 +1,21 @@
 function problem2(cryptogram) {
+  check(cryptogram);
   var answer;
   return answer;
 }
 
 module.exports = problem2;
+
+function checkLength(cryptogram) {
+  if (cryptogram.length > 1000 || cryptogram.length < 1)
+    throw new Error('길이는 1이상 1000이하여야 합니다.');
+}
+function checkChar(cryptogram) {
+  const smallEng = /^[a-z]*$/;
+  if (!smallEng.test(cryptogram))
+    throw new Error('문자는 소문자 알파벳이어야만 합니다!');
+}
+function check(cryptogram) {
+  checkLength(cryptogram);
+  checkChar(cryptogram);
+}
