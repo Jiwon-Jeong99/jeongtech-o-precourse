11568c0b6c72dd31f705cacd2067757d3a9d37cc
d0422 <rlfehd2013@naver.com>
2023-03-27 18:11:43 +0900

    feat: 예외사항; number가 1이상 1000이하의 자연수인지 검사하는 기능 추가

--- JavaScript/onBoarding/이동길/src/problem3.js
+++ JavaScript/onBoarding/이동길/src/problem3.js
@@ -1,6 +1,19 @@
 function problem3(number) {
+  check(number);
   var answer;
   return answer;
 }
 
 module.exports = problem3;
+function checkNumberAmount(number) {
+  if (number < 1 || number > 1000)
+    throw new Error('숫자는 1이상 1000이하여야합니다!');
+}
+function checkNumber(number) {
+  const num = /[0-9]/;
+  if (!num.test(number)) throw new Error('입력은 숫자여야합니다!');
+}
+function check(number) {
+  checkNumberAmount(number);
+  checkNumber(number);
+}
