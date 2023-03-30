ff16624a71be8aec2ba7eb5db2da164e4197884c
d0422 <rlfehd2013@naver.com>
2023-03-30 12:04:45 +0900

    feat: 아스키 코드 기반 문자 변형 기능 추가

--- JavaScript/onBoarding/이동길/src/problem4.js
+++ JavaScript/onBoarding/이동길/src/problem4.js
@@ -1,6 +1,18 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  return getReversedString(word);
 }
 
+function getReversedChar(char) {
+  const asciiCode = char.charCodeAt(0);
+  let newAscii;
+  if (asciiCode >= 65 && asciiCode <= 90) newAscii = 155 - asciiCode;
+  if (asciiCode >= 97 && asciiCode <= 122) newAscii = 219 - asciiCode;
+  if (newAscii === undefined) return char;
+  return String.fromCharCode(newAscii);
+}
+
+function getReversedString(str) {
+  const stringArr = str.split('');
+  return stringArr.map((char) => getReversedChar(char)).join('');
+}
 module.exports = problem4;
