10a6f790701b4e0bc018346b05c8b6b38a7122f3
d0422 <rlfehd2013@naver.com>
2023-03-30 12:31:11 +0900

    feat: 예외처리 돈이 1이상 1,000,000이하인 자연수인지 체크하는 기능 추가

--- JavaScript/onBoarding/이동길/src/problem5.js
+++ JavaScript/onBoarding/이동길/src/problem5.js
@@ -1,10 +1,11 @@
 function problem5(money) {
+  moneyCheck(money);
   return getMoneyCountArray(money);
 }
 
 function getMoneyCountArray(money) {
   const moneyStandard = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  const moneyArray = [0, 0, 0, 0, 0, 0, 0, 0];
+  const moneyArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   let index = 0;
   while (money != 0) {
     moneyArray[index] = Math.floor(money / moneyStandard[index]);
@@ -12,5 +13,11 @@ function getMoneyCountArray(money) {
     index++;
   }
   return moneyArray;
+}
+
+function moneyCheck(money) {
+  if (money <= 0 && money > 1000000) {
+    throw new Error('돈은 1원 이상 100만원 이하여야합니다!');
+  }
 }
 module.exports = problem5;
