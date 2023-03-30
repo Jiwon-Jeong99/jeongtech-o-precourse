2c4a3e4f9486f84e38825ebaecb6ad6cbea8057b
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-03-30 12:43:45 +0900

    [Feat] 문자 -> 아스키코드 변환 함수

--- JavaScript/onBoarding/정지원/src/problem4.js
+++ JavaScript/onBoarding/정지원/src/problem4.js
@@ -1,4 +1,14 @@
+// 문자 -> 아스키코드 변환
+const changeCharToAscii = (inputArray) => {
+  const asciiArr = inputArray.map((char) => char.charCodeAt());
+  return asciiArr;
+};
+
+// 청개구리로 아스키코드 변환
+// 아스키코드 -> 문자 변환
+
 function problem4(word) {
+  const wordArray = [...word];
   var answer;
   return answer;
 }
