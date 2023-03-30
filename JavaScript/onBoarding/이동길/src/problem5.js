76241e91540410450cfcc7663991307f0b6c24d3
d0422 <rlfehd2013@naver.com>
2023-03-30 12:25:18 +0900

    feat: 돈 단위를 기준으로 각 단위로 몇개로 변환되는지 구하는 기능 추가

--- JavaScript/onBoarding/이동길/src/problem5.js
+++ JavaScript/onBoarding/이동길/src/problem5.js
@@ -1,6 +1,16 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  return getMoneyCountArray(money);
 }
 
+function getMoneyCountArray(money) {
+  const moneyStandard = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const moneyArray = [0, 0, 0, 0, 0, 0, 0, 0];
+  let index = 0;
+  while (money != 0) {
+    moneyArray[index] = Math.floor(money / moneyStandard[index]);
+    money = money % moneyStandard[index];
+    index++;
+  }
+  return moneyArray;
+}
 module.exports = problem5;
