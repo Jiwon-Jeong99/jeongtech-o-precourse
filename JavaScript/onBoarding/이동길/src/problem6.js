aa14aa6b2499dda0dadfc6635986e5d857cd83d3
d0422 <rlfehd2013@naver.com>
2023-04-03 22:08:52 +0900

    feat: 같은 글자가 연속적으로 포함되는지 여부 확인하여 이메일을 추가하고 오름차순으로 정렬하는 기능 추가

--- JavaScript/onBoarding/이동길/src/problem6.js
+++ JavaScript/onBoarding/이동길/src/problem6.js
@@ -1,6 +1,43 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  return getEmailByDuplicatedNickName(forms);
 }
 
 module.exports = problem6;
+
+function getEmailByDuplicatedNickName(forms) {
+  const nickNames = forms.map((data) => data[1]);
+  const compareStringList = getCheckedStringList(nickNames);
+  const compareStringListObject = {};
+  compareStringList.forEach(
+    (compareString) =>
+      (compareStringListObject[compareString] = { count: 0, emailList: [] })
+  );
+  compareStringList.forEach((comapreString) => {
+    forms.forEach(([email, nickName]) => {
+      if (nickName.includes(comapreString)) {
+        compareStringListObject[comapreString].count++;
+        compareStringListObject[comapreString].emailList.push(email);
+      }
+    });
+  });
+
+  const duplicateEmail = new Set();
+  Object.values(compareStringListObject).forEach(({ count, emailList }) => {
+    if (count > 1) {
+      emailList.forEach((email) => {
+        duplicateEmail.add(email);
+      });
+    }
+  });
+  
+  return [...duplicateEmail].sort();
+}
+function getCheckedStringList(nickNames) {
+  const stringList = new Set();
+  nickNames.forEach((nickname) => {
+    for (let i = 0; i < nickname.length - 1; i++) {
+      stringList.add(nickname.substr(i, i + 2));
+    }
+  });
+  return stringList;
+}
