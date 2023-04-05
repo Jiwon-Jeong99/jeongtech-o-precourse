d08fc71c4d75d372371bfbeca72ab1aa9919cf16
d0422 <rlfehd2013@naver.com>
2023-04-05 22:44:57 +0900

    feat: 사용자별 친구 관계파악 기능 추가

--- JavaScript/onBoarding/이동길/src/problem7.js
+++ JavaScript/onBoarding/이동길/src/problem7.js
@@ -1,6 +1,25 @@
 function problem7(user, friends, visitors) {
   var answer;
+  const friendsListObject = getFriendsListObject(friends);
   return answer;
+}
+
+function getFriendsListObject(friends) {
+  const friendsListObject = {};
+  friends.forEach((people) => {
+    people.forEach((person) => {
+      if (friendsListObject[person] === undefined) {
+        friendsListObject[person] = new Set();
+      }
+    });
+  });
+
+  friends.forEach(([person1, person2]) => {
+    friendsListObject[person1].add(person2);
+    friendsListObject[person2].add(person1);
+  });
+
+  return friendsListObject;
 }
 
 module.exports = problem7;
