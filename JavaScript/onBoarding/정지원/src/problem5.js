33f975917130dd87f5431421e793a011aa0ddcd1
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-04-06 12:31:43 +0900

    [Feat] 몫과 나머지 구하는 함수 구현

--- JavaScript/onBoarding/정지원/src/problem5.js
+++ JavaScript/onBoarding/정지원/src/problem5.js
@@ -1,6 +1,15 @@
+function quo(money, cost) {
+  quotient = Math.floor(money / cost);
+  return quotient;
+}
+
+function remain(money, cost) {
+  remainder = money % cost;
+  return remainder;
+}
+
 function problem5(money) {
-  var answer;
-  return answer;
+  cost = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 }
 
 module.exports = problem5;
