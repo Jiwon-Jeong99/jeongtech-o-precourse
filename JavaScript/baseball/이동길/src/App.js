7aba0a061156d5f594db4056ffae10ed30b51153
d0422 <rlfehd2013@naver.com>
2023-05-04 01:52:44 +0900

    feat: baseBall

--- JavaScript/baseball/이동길/src/App.js
+++ JavaScript/baseball/이동길/src/App.js
@@ -1,5 +1,28 @@
+const Computer = require('./Computer');
+const View = require('./View');
 class App {
-  play() {}
+  play() {
+    View.startGame();
+    this.computer = new Computer();
+    View.Input(this.gameProgress.bind(this));
+  }
+
+  gameProgress(input) {
+    this.computer.updateState(input);
+    this.printResult();
+    if (this.computer.isWin()) {
+      View.win();
+      View.retry(this.play.bind(this));
+    }
+
+    View.Input(this.gameProgress.bind(this));
+  }
+
+  printResult() {
+    View.gameResult(this.computer.getState());
+  }
 }
 
 module.exports = App;
+const a = new App();
+a.play();
