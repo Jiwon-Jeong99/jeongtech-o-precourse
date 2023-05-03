7e5df309d3af1d8767de265d9406e30c34ec25d8
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-05-04 01:15:07 +0900

    [Feat] 컴퓨터의 random 숫자 3개 select 하기

--- JavaScript/baseball/정지원/src/App.js
+++ JavaScript/baseball/정지원/src/App.js
@@ -4,7 +4,17 @@ class App {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
   }
 
-  play() {}
+  selectRandomNum() {
+    randomNumArr = [];
+    for (i = 0; i < 3; i++) {
+      const randomNum = MissionUtils.Random.pickNumberInRange(1, 9);
+      randomNumArr.push(randomNum);
+    }
+  }
+
+  play() {
+    this.showGameStart();
+  }
 }
 
 module.exports = App;
