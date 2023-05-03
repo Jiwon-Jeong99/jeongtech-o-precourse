e8fc38dcafa8f5f454a237718bf6fd29ea9f1212
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-05-04 02:24:16 +0900

    [Feat] 같은 숫자, 같은 자리일 경우 스트라이크 함수 구현

--- JavaScript/baseball/정지원/src/Model.js
+++ JavaScript/baseball/정지원/src/Model.js
@@ -1,4 +1,18 @@
 class Model {
+  constructor(inputArr, randomArr) {
+    this.inputArr = inputArr;
+    this.randomArr = randomArr;
+  }
+
+  getStrike() {
+    let count = 0;
+    for (let i = 0; i < 3; i++) {
+      if (this.inputArr[i] === this.randomArr[i]) count += 1;
+      return count;
+    }
+  }
+
+
 
 }
 
